8c61152eab8ed6d30a3736a7ce36c2b598e0f4f7
Alexander Konukhov <jesusmisha@gmail.com>
2023-04-20 22:08:37 +0300

    added comments

--- homeworks/homework2/CodeCheck1.java
+++ homeworks/homework2/CodeCheck1.java
@@ -1,3 +1,6 @@
+// Если необходимо, исправьте данный код
+// (задание 2 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+
 package homeworks.homework2;
 
 import java.io.IOException;
--- homeworks/homework2/CodeCheck2.java
+++ homeworks/homework2/CodeCheck2.java
@@ -1,3 +1,6 @@
+// Дан следующий код, исправьте его там, где требуется
+// (задание 3 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+
 package homeworks.homework2;
 
 public class CodeCheck2 {
